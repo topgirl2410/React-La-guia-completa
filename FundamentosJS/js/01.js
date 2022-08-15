55f49937f07a5006f15e0dc0d59d09e67bc5e955
topgirl2410 <maria.topgirl82@gmail.com>
2022-08-15 19:47:31 +0200

    let y const

---
+++ FundamentosJS/js/01.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Variables - let
+let cliente = "Mari"; // String
+let precioDescuento = .25; // Numero
+let inicioSesion = false; // Boolean
+
+
+// Puedes reasignar
+cliente = "María";
+inicioSesion = true;
+
+console.log(cliente);
+console.log(precioDescuento);
+console.log(inicioSesion);
+
+// Pueden iniciar sin un valor
+let precio;
+
+precio = 1000;
+
+console.log(precio)
+
+
+
---
+++ FundamentosJS/js/02.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Variables - const
+
+const cliente = "Pedro";
+const precioDescuento = .25;
+
+
+
+// No se puede reasignar
+
+/*cliente = 20; // Error*/
+
+// No pueden iniciar sin un valor
+
+/* const precio 
+
+console.log(precio)*/
+
+console.log(cliente);
+console.log(precioDescuento);
