31ea6f6e36aee038588f535fc1bad214fd355ca9
topgirl2410 <maria.topgirl82@gmail.com>
2022-08-15 20:05:12 +0200

    fundamentos js

---
+++ FundamentosJS/js/00-datosImportantes.js
@@ -0,0 +1,24 @@
+// Las reglas de las variables
+
+/*
+ * Las variables no pueden iniciar con números o caracteres especiales (Guion bajo si esta permitido);
+ * Las variables no pueden tener espacios.
+ *
+ * JavaScript recomienda el uso de camelCase
+ */
+
+// Tipos de Datos y Valores en JavaScript
+
+/*
+ * Existen tipos de estructuras y datos en Javascript, son 9 en total.
+ * 
+ * 01- Undefined
+ * 02- Boolean
+ * 03- Number
+ * 04- String
+ * 05- Funtion
+ * 06- Symbol
+ * 07- Big Int
+ * 08- Object
+ * 09- Null
+ */
