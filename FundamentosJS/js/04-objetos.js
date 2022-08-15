ecefd11024b158cae301aba1bee5bc0eb7075626
topgirl2410 <maria.topgirl82@gmail.com>
2022-08-15 22:16:57 +0200

    introduccionn a objetos JS

--- FundamentosJS/js/04-objetos.js
+++ FundamentosJS/js/04-objetos.js
@@ -14,7 +14,20 @@ console.log(producto.precio);
 console.log(producto.disponible);
 
 // Destructuring
+
 const { nombre, precio, disponible } = producto;
 console.log(nombre);
-console.log(precio);
+console.log(precio); producto
 console.log(disponible);
+
+// Obejct Literal Enhacement
+
+const autentificar = true;
+const usuario = "Maria";
+
+const nuevoObjeto = {
+    autentificar,
+    usuario
+};
+
+console.table(nuevoObjeto);
