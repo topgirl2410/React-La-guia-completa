a83bc4b3a86f538b37d1e6132388048bed218712
topgirl2410 <maria.topgirl82@gmail.com>
2022-08-15 23:07:16 +0200

    reescribir un valor y agregar un nuevo valor

---
+++ FundamentosJS/js/05-manipulacionObjetos.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Objetos - Manipulación
+
+const producto = {
+    nombre: "Televisor LCD 45 Pulgadas",
+    precio: 450,
+    disponible: true
+}
+
+
+// Reescribir un valor 
+
+producto.nombre = "Monitor Curvo LCD"
+
+
+// Agregar un valor nuevo
+
+producto.imagen = "imagen.jpg"
+
+
+
+console.table(producto);
